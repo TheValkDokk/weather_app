e65eaf71bc4250406cca6990c396d7ad8f6ba4d6
Valk Dokk <thevalkdokk@gmail.com>
2025-04-04 14:52:57 +0700

    Add PermissionService for managing app permissions
    - Implemented PermissionService to handle permission requests and status checks.
    - Added methods to request permissions and check if they are granted, denied, limited, or permanently denied.
    - Created unit tests for PermissionService to validate permission handling logic and mock method channel responses.

---
+++ lib/core/services/permission_service.dart
@@ -0,0 +1,32 @@
+import 'package:injectable/injectable.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+@singleton
+class PermissionService {
+  List<Permission> permissions = [Permission.locationWhenInUse];
+
+  Future<List<PermissionStatus>> requestPermissions() async {
+    final list = <PermissionStatus>[];
+    for (var permission in permissions) {
+      final result = await permission.request();
+      list.add(result);
+    }
+    return list;
+  }
+
+  Future<bool> isGranted(Permission permission) async {
+    return await permission.status.isGranted;
+  }
+
+  Future<bool> isPermanentlyDenied(Permission permission) async {
+    return await permission.status.isPermanentlyDenied;
+  }
+
+  Future<bool> isDenied(Permission permission) async {
+    return await permission.status.isDenied;
+  }
+
+  Future<bool> isLimited(Permission permission) async {
+    return await permission.status.isLimited;
+  }
+}
---
+++ test/core/services/permission_service_test.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:weather_app/core/services/permission_service.dart';
+
+class MockPermissionService extends Mock implements PermissionService {}
+
+void main() {
+  late PermissionService permissionService;
+
+  setUp(() {
+    TestWidgetsFlutterBinding.ensureInitialized();
+
+    permissionService = PermissionService();
+  });
+
+  void mockMethodChannel({required String method, required dynamic result}) {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(
+          MethodChannel('flutter.baseflow.com/permissions/methods'),
+          (MethodCall methodCall) async {
+            return Future.value(result);
+          },
+        );
+  }
+
+  test('isDenied returns Success with denied status', () async {
+    mockMethodChannel(method: 'requestPermissions', result: 0);
+    final result = await permissionService.isDenied(Permission.location);
+    expect(result, true);
+  });
+
+  test('isGranted returns Success with granted status', () async {
+    mockMethodChannel(method: 'requestPermissions', result: 1);
+    final result = await permissionService.isGranted(Permission.location);
+    expect(result, true);
+  });
+
+  test('isLimited returns Success with limited status', () async {
+    mockMethodChannel(method: 'requestPermissions', result: 3);
+    final result = await permissionService.isLimited(Permission.location);
+    expect(result, true);
+  });
+
+  test(
+    'isPermanentlyDenied returns Success with permanently denied status',
+    () async {
+      mockMethodChannel(method: 'requestPermissions', result: 4);
+      final result = await permissionService.isPermanentlyDenied(
+        Permission.location,
+      );
+      expect(result, true);
+    },
+  );
+
+  test('requestPermission returns Success with all denied status', () async {
+    mockMethodChannel(method: 'requestPermissions', result: {0: 0});
+    final result = await permissionService.requestPermissions();
+    expect(result, [PermissionStatus.denied]);
+  });
+}
