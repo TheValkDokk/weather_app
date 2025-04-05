fffbda576484e8c1b1e3fee873bd1fa51b5d7ffa
Valk Dokk <thevalkdokk@gmail.com>
2025-04-05 17:48:33 +0700

    feat: enhance location data source implementation and add unit tests
    - Updated LocationDataSource to include error handling for location services and permissions.
    - Refactored getPlacemarks method to accept latitude and longitude parameters.
    - Introduced unit tests for LocationDataSourceImpl, covering current location retrieval and placemark fetching scenarios.

--- lib/features/location/data/datasource/location_datasource.dart
+++ lib/features/location/data/datasource/location_datasource.dart
@@ -1,29 +1,74 @@
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:injectable/injectable.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:weather_app/core/app.dart';
+import 'package:weather_app/features/location/data/datasource/geocoding_datasource.dart';
 
 abstract class LocationDataSource {
   Future<Position> getCurrentLocation();
-  Future<List<Placemark>> getPlacemarks(Position position);
+  Future<List<Placemark>> getPlacemarks({
+    required double latitude,
+    required double longitude,
+  });
 }
 
 @Singleton(as: LocationDataSource)
 class LocationDataSourceImpl implements LocationDataSource {
   final PermissionService permissionService;
+  final GeocodingService geocodingService;
   final GeolocatorPlatform geolocator;
 
-  LocationDataSourceImpl(this.geolocator, this.permissionService);
+  LocationDataSourceImpl(
+    this.geolocator,
+    this.permissionService,
+    this.geocodingService,
+  );
 
   @override
   Future<Position> getCurrentLocation() async {
-    // TODO: implement getCurrentLocation
-    throw UnimplementedError();
+    final isServiceEnabled = await geolocator.isLocationServiceEnabled();
+    if (!isServiceEnabled) {
+      throw Failure(
+        message: 'Location services are disabled. Please enable them.',
+        id: 'location_service_disabled',
+      );
+    }
+
+    final status = await permissionService.isGranted(
+      Permission.locationWhenInUse,
+    );
+
+    if (!status) {
+      throw Failure(
+        message: 'Location permission is not granted. Please grant it.',
+        id: 'location_permission_not_granted',
+      );
+    }
+
+    //if last known position is available, use it (it's faster than getting current position)
+    try {
+      final lastKnownPosition = await geolocator.getLastKnownPosition();
+      if (lastKnownPosition != null) {
+        return lastKnownPosition;
+      }
+    } catch (e) {
+      //no last known position, ignore
+    }
+
+    return await geolocator.getCurrentPosition(
+      locationSettings: LocationSettings(
+        //checking temp in city area so medium accuracy is sufficient
+        accuracy: LocationAccuracy.medium,
+      ),
+    );
   }
 
   @override
-  Future<List<Placemark>> getPlacemarks(Position position) async {
-    // TODO: implement getPlacemarks
-    throw UnimplementedError();
+  Future<List<Placemark>> getPlacemarks({
+    required double latitude,
+    required double longitude,
+  }) {
+    return geocodingService.placemarkFromCoordinates(latitude, longitude);
   }
 }
