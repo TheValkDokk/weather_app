615f82e6d7d0a9a2f061d730adde8ca80673c829
Valk Dokk <thevalkdokk@gmail.com>
2025-04-06 22:59:45 +0700

    feat: implement WeatherRemoteDataSource for fetching current weather
    - Added WeatherRemoteDataSource interface and its implementation, WeatherRemoteDataSourceImpl, to handle API calls for current weather data.
    - Implemented error handling with ServerFailure for failed API responses.
    - Created unit tests for WeatherRemoteDataSourceImpl to validate successful data retrieval and error scenarios.

---
+++ lib/features/weather/data/datasource/remote/open_weather_datasource.dart
@@ -0,0 +1,44 @@
+import 'package:injectable/injectable.dart';
+import 'package:weather_app/core/errors/server_failure.dart';
+import 'package:weather_app/core/services/dio_service.dart';
+import 'package:weather_app/core/services/env_service.dart';
+import 'package:weather_app/features/weather/data/models/weather_model.dart';
+
+abstract class WeatherRemoteDataSource {
+  Future<WeatherModel> getCurrentWeather(double lat, double lon);
+}
+
+@LazySingleton(as: WeatherRemoteDataSource)
+class WeatherRemoteDataSourceImpl implements WeatherRemoteDataSource {
+  final DioService dioService;
+  final EnvService envService;
+
+  late final String apiKey;
+  WeatherRemoteDataSourceImpl(this.dioService, this.envService) {
+    apiKey = envService.get('OPEN_WEATHER_API_KEY');
+  }
+
+  @override
+  Future<WeatherModel> getCurrentWeather(double lat, double lon) async {
+    final api = '/data/3.0/onecall';
+    final response = await dioService.get(
+      api,
+      queryParameters: {
+        'lat': lat.toString(),
+        'lon': lon.toString(),
+        'units': 'metric',
+        'appid': apiKey,
+        'exclude': 'minutely,alerts,hourly',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return WeatherModel.fromJson(response.data);
+    } else {
+      throw ServerFailure(
+        'Failed to fetch current weather ${response.data}',
+        response.statusCode ?? 500,
+      );
+    }
+  }
+}
