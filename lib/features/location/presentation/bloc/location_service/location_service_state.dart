e6f8d920e84158240eb08c569f374ac1ea3f6f6f
Chánh Đức <91455763+TheValkDokk@users.noreply.github.com>
2025-04-09 14:56:02 +0700

    Fix/improve location service test (#9)
    * refactor: rename loading state in LocationServiceState and update WeatherBloc handling
    
    - Renamed loading state in LocationServiceState from `Loading` to `LocationServiceLoading` for clarity.
    - Updated WeatherBloc to handle the new loading state, ensuring proper event emissions based on location service status.
    - Modified tests to reflect the inclusion of loading state emissions during location service checks.
    
    * chore: add INTERNET permission to AndroidManifest.xml
    
    - Added INTERNET permission to AndroidManifest.xml to enable network access for the application.

--- lib/features/location/presentation/bloc/location_service/location_service_state.dart
+++ lib/features/location/presentation/bloc/location_service/location_service_state.dart
@@ -3,7 +3,7 @@ part of 'location_service_bloc.dart';
 @freezed
 class LocationServiceState with _$LocationServiceState {
   const factory LocationServiceState.initial() = _Initial;
-  const factory LocationServiceState.loading() = Loading;
+  const factory LocationServiceState.loading() = LocationServiceLoading;
   const factory LocationServiceState.locationServiceNotEnabled() =
       LocationServiceNotEnabled;
   const factory LocationServiceState.locationServiceEnabled(Position position) =
